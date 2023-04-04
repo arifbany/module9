ac880dc76e1811b0ec708be0e7efec93d7d7d408
Md Arif Hassan <125501103+arifbany@users.noreply.github.com>
2023-04-04 23:53:18 +0600

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,20 @@
+
+
+<div class="col-lg-12">
+                  <div class="sidebar-item tags">
+                    <div class="sidebar-heading">
+                      <h2>Tag Clouds</h2>
+                    </div>
+                    <div class="content">
+                      <ul>
+                        <li><a href="post-details2.php">Lifestyle</a></li>
+                        <li><a href="post-details2.php">Creative</a></li>
+                        <li><a href="post-details2.php">HTML5</a></li>
+                        <li><a href="post-details2.php">Inspiration</a></li>
+                        <li><a href="post-details2.php">Motivation</a></li>
+                        <li><a href="post-details2.php">PSD</a></li>
+                        <li><a href="#post-details2.php">Responsive</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
